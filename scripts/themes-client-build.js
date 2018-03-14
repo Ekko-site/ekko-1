8b5bbbd77f7913b9f3d36bb13b170d6759fc8266
Ben Howdle <benhowdle89@gmail.com>
2018-03-14 19:32:45 +0000

    building themes

---
+++ scripts/themes-client-build.js
@@ -0,0 +1,27 @@
+import fse from "fs-extra";
+import junk from "junk";
+import browserify from "browserify";
+import babelify from "babelify";
+
+const build = async theme => {
+  return new Promise((resolve, reject) => {
+    const themePath = `${__dirname}/../src/themes/${theme}`;
+    browserify(`${themePath}/js/client.js`)
+      .transform(babelify)
+      .bundle()
+      .pipe(fse.createWriteStream(`${themePath}/js/built.js`));
+    return resolve();
+  });
+};
+
+(async () => {
+  try {
+    const themeDirs = await fse.readdir(`${__dirname}/../src/themes/`);
+    const themes = themeDirs
+      .filter(junk.not)
+      .filter(d => !["components", "layouts"].includes(d));
+    const built = await Promise.all(themes.map(build));
+  } catch (error) {
+    console.error(error);
+  }
+})();
