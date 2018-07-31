86c0a017cf6f3a841ec395627449a9e231d29e47
Ben Howdle <benhowdle89@gmail.com>
2018-07-31 21:01:12 +0100

    stripe elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { StripeProvider } from "react-stripe-elements";
 import createHistory from "history/createBrowserHistory";
 import ReduxToastr from "react-redux-toastr";
 
@@ -10,19 +11,22 @@ import Switcher from "@/containers/switcher";
 
 const history = createHistory();
 const { store } = configureStore(history, window.REDUX_STATE);
+const config = process.env;
 
 class Main extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <div>
-          <Switcher />
-          <ReduxToastr
-            timeOut={4000}
-            newestOnTop={false}
-            position="top-right"
-          />
-        </div>
+        <StripeProvider apiKey={config.REACT_APP_STRIPE_TOKEN}>
+          <div>
+            <Switcher />
+            <ReduxToastr
+              timeOut={4000}
+              newestOnTop={false}
+              position="top-right"
+            />
+          </div>
+        </StripeProvider>
       </Provider>
     );
   }
--- client/src/components/settings/stripe-card.jsx
+++ client/src/components/settings/stripe-card.jsx
@@ -1,105 +1,84 @@
-import Script from 'react-load-script'
-import React from 'react'
-
-import Loading from '@/components/loading'
-
-const config = process.env
-const Stripe = window.Stripe
+import React from "react";
+import {
+  CardNumberElement,
+  CardExpiryElement,
+  CardCVCElement,
+  injectStripe
+} from "react-stripe-elements";
+import Loading from "@/components/loading";
 
 class StripeCard extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            stripeLoaded: false,
-            showUpdateCardDetails: false
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      showUpdateCardDetails: false
+    };
+  }
+
+  stripeResponseHandler = response => {
+    if (response && response.id) {
+      return this.props.cardUpdate(response.id, response.card);
     }
+    return this.props.onError(response.error.message);
+  };
 
-    stripeLoaded() {
-        Stripe.setPublishableKey(config.REACT_APP_STRIPE_TOKEN)
-        this.setState({ stripeLoaded: true })
-    }
+  createToken = async form => await this.props.stripe.createToken(form);
 
-    stripeResponseHandler = (status, response) => {
-        if(response && response.id) {
-            return this.props.cardUpdate(response.id, response.card)
-        }
-        return this.props.onError(response.error.message)
-    }
+  formSubmitted = async event => {
+    event.preventDefault();
+    const { token } = await this.createToken();
+    this.props.onTokenFetch();
+    this.stripeResponseHandler(token);
+    return false;
+  };
 
-    formSubmitted = event => {
-        event.preventDefault()
-        this.props.onTokenFetch()
-        Stripe.card.createToken(event.target, this.stripeResponseHandler)
-        return false
-    }
+  render() {
+    const { user, cardUpdated, cardUpdating } = this.props;
+    return (
+      <div
+        style={{
+          marginTop: "2em"
+        }}>
+        <div className="half-mb">
+          <span className="caps-title">Card in use:</span>
+          <p className="mini faded">
+            {user.StripeCustomerId.brand} **** **** ****{" "}
+            {user.StripeCustomerId.last4}
+          </p>
+        </div>
+        {!this.state.showUpdateCardDetails && (
+          <p className="half-mb">
+            Update your{" "}
+            <span
+              className="text-button"
+              onClick={() =>
+                this.setState({
+                  showUpdateCardDetails: true
+                })
+              }>
+              card details?
+            </span>
+          </p>
+        )}
+        {cardUpdated && <p>Card details updated!</p>}
+        {this.state.showUpdateCardDetails &&
+          !cardUpdated && (
+            <form className="form" onSubmit={this.formSubmitted}>
+              <CardNumberElement />
+              <CardExpiryElement />
+              <CardCVCElement />
 
-    render() {
-        if(!this.state.stripeLoaded) {
-            return (
-                <Script
-                    url='https://js.stripe.com/v2/'
-                    onLoad={() => this.stripeLoaded() }
-                />
-            )
-        }
-        const { user, cardUpdated, cardUpdating } = this.props
-        return (
-            <div style={{
-                marginTop: '2em'
-            }}>
-                <div className="half-mb">
-                    <span className="caps-title">Card in use:</span>
-                    <p className="mini faded">
-                        { user.StripeCustomerId.brand } **** **** **** { user.StripeCustomerId.last4 }
-                    </p>
-                </div>
-                {(!this.state.showUpdateCardDetails) && (
-                    <p className="half-mb">Update your <span className="text-button" onClick={() => this.setState({
-                        showUpdateCardDetails: true
-                    })}>card details?</span></p>
-                )}
-                {(cardUpdated) && (
-                    <p>Card details updated!</p>
-                )}
-                {(this.state.showUpdateCardDetails && !cardUpdated) && (
-                    <form className="form" onSubmit={this.formSubmitted}>
-
-                      <div className="form-row i-w-m">
-                        <label className="mini">
-                          <span>Card Number</span>
-                        </label>
-                        <input type="text" size="20" data-stripe="number" />
-                      </div>
-
-                      <div className="form-row i-w-m">
-                        <label className="mini">
-                          <span>Expiration (MM/YY)</span>
-                        </label>
-                        <input type="text" size="2" data-stripe="exp_month" />
-                        <span> / </span>
-                        <input type="text" size="2" data-stripe="exp_year" />
-                      </div>
-
-                      <div className="form-row big-mb">
-                        <label className="mini">
-                          <span>CVC</span>
-                        </label>
-                        <input type="text" size="4" data-stripe="cvc" />
-                      </div>
-
-                      {(cardUpdating) && (
-                          <Loading />
-                      )}
-                      {(!cardUpdating) && (
-                          <button className="butt butt--yellow half-mt">Update card details</button>
-                      )}
-                    </form>
-                )}
-            </div>
-        )
-    }
-
+              {cardUpdating && <Loading />}
+              {!cardUpdating && (
+                <button className="butt butt--yellow half-mt">
+                  Update card details
+                </button>
+              )}
+            </form>
+          )}
+      </div>
+    );
+  }
 }
 
-export default StripeCard
+export default injectStripe(StripeCard);
