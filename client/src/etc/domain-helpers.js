0efc6468a852f93d824d49ef7529d75572a4a53c
Ben Howdle <benhowdle89@gmail.com>
2018-08-12 12:31:22 +0100

    domain https

--- client/src/etc/domain-helpers.js
+++ client/src/etc/domain-helpers.js
@@ -1,15 +1,15 @@
-const config = process.env
+const config = process.env;
 
 export default {
-    getDomain: (page, userPages) => {
-        const pageId = page.id
-        const pageDomain = userPages.find(u => u.PageId == pageId)
-        if(!pageDomain){
-            return `${config.REACT_APP_API_URL}/s/${page.facebookPageId}`
-        }
-        if(pageDomain.domainId || pageDomain.dns_resolved){
-            return `http://${pageDomain.domain}`
-        }
-        return `${config.REACT_APP_API_URL}/s/${page.facebookPageId}`
+  getDomain: (page, userPages) => {
+    const pageId = page.id;
+    const pageDomain = userPages.find(u => u.PageId == pageId);
+    if (!pageDomain) {
+      return `${config.REACT_APP_API_URL}/s/${page.facebookPageId}`;
     }
-}
+    if (pageDomain.domainId || pageDomain.dns_resolved) {
+      return `https://${pageDomain.domain}`;
+    }
+    return `${config.REACT_APP_API_URL}/s/${page.facebookPageId}`;
+  }
+};
