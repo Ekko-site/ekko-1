2ea87cf240848a9f6c3c31e81180703ac85dc78d
Ben Howdle <benhowdle89@gmail.com>
2018-08-12 14:08:54 +0100

    hostname debug

--- src/app.js
+++ src/app.js
@@ -51,13 +51,6 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  const { hostname } = url.parse("https://" + req.headers.host);
-  logger.info(
-    "hostname",
-    hostname,
-    req.headers,
-    hostname.match(/(ekko|localhost)/)
-  );
   logger.info("REQUEST", req.url, req.method);
   next();
 });
