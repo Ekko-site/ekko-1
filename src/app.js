34b6bd4c1621fe1b7fce2c939aed7ea85e48c43d
Ben Howdle <benhowdle89@gmail.com>
2018-08-12 13:32:21 +0100

    hostname debug

--- src/app.js
+++ src/app.js
@@ -76,7 +76,6 @@ app.use((req, res, next) => {
   if (!hostname.match(/(ekko|localhost)/)) {
     return fetchSiteByHostname({ req, res, hostname });
   }
-  next();
 });
 
 app.use("/api/:controller", (req, res, next) => {
