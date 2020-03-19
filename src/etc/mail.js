0623a45bdf6294e3034877db9e5c2ea23d35dc9e
Ben Howdle <benhowdle89@gmail.com>
2020-03-19 21:10:42 +0000

    client build

--- src/etc/mail.js
+++ src/etc/mail.js
@@ -1,45 +1,47 @@
-import Sendgrid from 'sendgrid-web'
-import { logger } from '@/etc/logger'
-import messages from '@/etc/emails'
-import renderEmail from '@/etc/emails/render'
+import Sendgrid from "sendgrid-web";
+import { logger } from "@/etc/logger";
+import messages from "@/etc/emails";
+import renderEmail from "@/etc/emails/render";
 
 const sendgrid = new Sendgrid({
-    user: process.env.SENDGRID_USERNAME,
-    key: process.env.SENDGRID_PASSWORD
-})
+  user: process.env.SENDGRID_USERNAME,
+  key: process.env.SENDGRID_PASSWORD
+});
 
-const send = ({
-    to,
-    type,
-    data = {}
-}) => {
-    let message = messages[type]
-    if(!message) {
-        return
-    }
-    let { text, subject } = message(data)
-    const html = renderEmail({
-        content: text,
-        title: subject
-    })
-    if(!to) {
-        return
-    }
-    logger.info('Sending email to, with subject', to, subject, text)
-    return process.env.NODE_ENV == 'production' && sendgrid.send({
+const send = ({ to, type, data = {} }) => {
+  let message = messages[type];
+  if (!message) {
+    return;
+  }
+  let { text, subject } = message(data);
+  const html = renderEmail({
+    content: text,
+    title: subject
+  });
+  if (!to) {
+    return;
+  }
+  logger.info("Sending email to, with subject", to, subject, text);
+  return (
+    process.env.NODE_ENV == "production" &&
+    sendgrid.send(
+      {
         to,
-        from: 'hello@ekko.site',
-        fromname: 'Ben from Ekko',
+        from: "hello@ekko.site",
+        fromname: "Ben from Ekko",
         subject,
         html
-    }, (err, json) => {
+      },
+      (err, json) => {
         if (err) {
-            return console.error(err)
+          return console.error(err);
         }
-        logger.info('Mail response', to, json)
-    })
-}
+        logger.info("Mail response", to, json);
+      }
+    )
+  );
+};
 
 export default {
-    send
-}
+  send
+};
